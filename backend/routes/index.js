0947bc7f5c19454fce49c2bd590a0d1485ce0a6f
kmoin1309 <128276854+kmoin1309@users.noreply.github.com>
2024-06-09 13:30:37 +0530

    Added Account Schema and optimised code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,11 @@
-// backend/index.js
+// backend/user/index.js
 const express = require("express");
-const rootRouter = require("./routes/index");
 const userRouter = require("./user");
-const router = require("..");
+const accountRouter = require("./account");
 
+const router = express.Router();
 
-
-router.use("user",userRouter);
+router.use("/user", userRouter);
+router.use("/account", accountRouter);
 
 module.exports = router;
-
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,9 +3,10 @@ const express = require("express");
 
 const router = express.Router();
 const zod = require("zod");
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
+const { authMiddleware } = require("../middleware");
 
 const signupBody = zod.object({
   username: zod.string().email(),
@@ -39,6 +40,11 @@ router.post("/signup", async (req, res) => {
     lastName: req.body.lastName,
   });
   const userId = user._id;
+
+  await Account.create({
+    userId,
+    balance: 1 + Math.random() * 10000,
+  });
 
   const token = jwt.sign(
     {
@@ -90,6 +96,28 @@ router.post("/signin", async (req, res) => {
   });
 });
 
+const updateBody = zod.object({
+  password: zod.string().optional(),
+  firstName: zod.string().optional(),
+  lastName: zod.string().optional(),
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateBody.safeParse(req.body);
+  if (!success) {
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+  }
+
+  await User.updateOne(req.body, {
+    id: req.userId,
+  });
+
+  res.json({
+    message: "Updated successfully",
+  });
+});
 
 router.get("/bulk", async (req, res) => {
   const filter = req.query.filter || "";
@@ -118,4 +146,5 @@ router.get("/bulk", async (req, res) => {
     })),
   });
 });
+
 module.exports = router;
